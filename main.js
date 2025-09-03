0c6c92e0ba051adcbb2fc6884e9aca911ac29a3e
marrttao <marrttaoburberry@gmail.com>
2025-09-03 22:57:25 +0300

    completed task 1

---
+++ main.js
@@ -0,0 +1,13 @@
+const age = prompt("Enter your age:");
+if (age < 18) {
+    alert("You haven't access");
+}
+else if ( age >=18 && age <=60) {
+    alert("Welcome!");
+}
+else {
+    alert("You need to get a rest");
+}
+
+
+`
