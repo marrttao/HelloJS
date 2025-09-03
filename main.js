83273dbf1d16dfe4c71fa9e435b2310457c125a2
marrttao <marrttaoburberry@gmail.com>
2025-09-03 23:37:20 +0300

    completed

---
+++ main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    for(let i = 1; i <= 50; i++) {
+        if (i % 2 === 0) {
+            document.querySelector('.output').textContent += i + ' ';
+        }
+    }
+});
